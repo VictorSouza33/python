eda38fbf88d338e6551e247fee1312e16c2176bf
unknown <victorsouzabill@gmail.com>
2022-05-23 21:54:45 -0300

    versão final

---
+++ produto.py
@@ -0,0 +1,11 @@
+#representa uma entidade do banco de dados!
+
+class Produto:
+    def __init__(self, descricao, preco, quantidade,id):
+        self.id = id
+        self.descricao = descricao
+        self.preco = preco
+        self.quantidade = quantidade
+
+
+
